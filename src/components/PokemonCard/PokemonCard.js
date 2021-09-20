503f66e0ecadb05067e991b5040608dd84c23bde
Konton022 <42418105+Konton022@users.noreply.github.com>
2021-09-21 00:15:35 +0500

    add folders

--- src/components/PokemonCard/PokemonCard.js
+++ src/components/PokemonCard/PokemonCard.js
@@ -1,5 +1,5 @@
 // import { useState } from "react";
-import classNames from "classnames";
+import cn from "classnames";
 
 import s from "./style.module.css";
 import cardBackSide from "../../assets/card-back-side.jpeg";
@@ -13,6 +13,8 @@ const PokemonCard = ({
   name,
   id,
   active,
+  minimize,
+  className,
   handleId,
 }) => {
   const handleClick = () => {
@@ -21,40 +23,37 @@ const PokemonCard = ({
   };
 
   return (
-    <div className={s.root} onClick={handleClick}>
-      <div className={classNames(s.pokemonCard, { [s.active]: active })}>
-        <div className={s.cardFront}>
-          <div className={classNames(s.wrap, s.front)}>
-            <div className={classNames(s.pokemon, s[type])}>
-              <div className={s.values}>
-                <div className={classNames(s.count, s.top)}>{values.top}</div>
-                <div className={classNames(s.count, s.right)}>
-                  {values.right}
-                </div>
-                <div className={classNames(s.count, s.bottom)}>
-                  {values.bottom}
-                </div>
-                <div className={classNames(s.count, s.left)}>{values.left}</div>
-              </div>
-              <div className={s.imgContainer}>
-                <img src={img} alt={name} />
-              </div>
+    <div
+      className={cn(className, s.pokemonCard, { [s.active]: active })}
+      onclick={handleClick}
+    >
+      <div className={s.cardFront}>
+        <div className={cn(s.wrap, s.front)}>
+          <div className={cn(s.pokemon, s[type])}>
+            <div className={s.values}>
+              <div className={cn(s.count, s.top)}>{values.top}</div>
+              <div className={cn(s.count, s.right)}>{values.right}</div>
+              <div className={cn(s.count, s.bottom)}>{values.bottom}</div>
+              <div className={cn(s.count, s.left)}>{values.left}</div>
+            </div>
+            <div className={s.imgContainer}>
+              <img src={img} alt={name} />
+            </div>
+            {!minimize && (
               <div className={s.info}>
                 <span className={s.number}>#{id}</span>
                 <h3 className={s.name}>{name}</h3>
-                <small className={s[type]}>
+                <small className={s.type}>
                   Type: <span>{type}</span>
                 </small>
               </div>
-            </div>
+            )}
           </div>
         </div>
+      </div>
 
-        <div className={s.cardBack}>
-          <div className={classNames(s.wrap, s.back)}>
-            <img src={cardBackSide} alt="Сard Backed" />
-          </div>
-        </div>
+      <div className={s.cardBack}>
+        <div className={cn(s.wrap, s.back)} />
       </div>
     </div>
   );
