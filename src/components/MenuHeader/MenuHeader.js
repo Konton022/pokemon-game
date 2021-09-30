a37cd4ac01c689d62d2ea8e9c9d0698bc3f9a15d
Konton022 <42418105+Konton022@users.noreply.github.com>
2021-10-01 00:05:42 +0500

    done 10-1

--- src/components/MenuHeader/MenuHeader.js
+++ src/components/MenuHeader/MenuHeader.js
@@ -5,15 +5,25 @@ import NavBar from "../NavBar/NavBar";
 
 const MenuHeader = ({ bgActive }) => {
   const [active, setActive] = useState(null);
+  const [isOpenModal, setOpenModal] = useState(null);
 
   const handleClickButton = () => {
-    setActive(prevState => !prevState);
+    setActive((prevState) => !prevState);
     // console.log("### active: ", active);
   };
+  const handleClickLogin = () => {
+    setOpenModal((prevState) => !prevState);
+  };
+
   return (
     <div>
       <Menu isActive={active} />
-      <NavBar isActive={active} bgActive={bgActive} onClickButton={handleClickButton} />
+      <NavBar
+        isActive={active}
+        bgActive={bgActive}
+        onClickButton={handleClickButton}
+        onClickLogin={handleClickLogin}
+      />
     </div>
   );
 };
