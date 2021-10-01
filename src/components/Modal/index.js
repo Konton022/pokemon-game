87f515de7dd76e14b3bde0ce42e04324e474d501
Koston022 <koston022@mail.ru>
2021-10-01 12:42:12 +0500

    done 10-3

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,13 +1,27 @@
+import { useRef } from "react";
 import cn from "classnames";
 
 import s from "./style.module.css";
 const Modal = ({ isOpen, title, children, onCloseModal }) => {
+  const modalEl = useRef()
+
   const handleCloseModal = () => {
     onCloseModal && onCloseModal(false);
   };
+
+  const handleClickRoot = (event) => {
+    if (!modalEl.current.contains(event.target)) {
+      handleCloseModal();
+    }
+  }
   return (
-    <div className={cn(s.root, { [s.open]: isOpen })}>
-      <div className={cn(s.modal)}>
+    <div
+      className={cn(s.root, { [s.open]: isOpen })}
+      onClick={handleClickRoot}>
+      <div
+        ref={modalEl}
+        className={cn(s.modal)}
+      >
         <div className={cn(s.head)}>
           {title}
           <span className={cn(s.btnClose)} onClick={handleCloseModal}></span>
